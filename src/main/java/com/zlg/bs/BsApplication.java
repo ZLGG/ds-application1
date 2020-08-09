57e52106ef0f0979a2c389222c5ac0e936cb459b
zhengfeng.zlg@dtyunxi.com <zhengfeng.zlg@dtyunxi.com>
2020-08-09 20:24:15 +0800

    修改启动类

--- src/main/java/com/zlg/bs/BsApplication.java
+++ src/main/java/com/zlg/bs/BsApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BsApplication {
 
     public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("");
         SpringApplication.run(BsApplication.class, args);
     }
 
